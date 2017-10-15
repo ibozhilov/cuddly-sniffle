d74f4e150bfe8c70425e78aa5d29bc859b3edff0
Ivan Bozhilov <ivanbojilov@gmail.com>
2017-10-15 13:59:42 +0300

    Refactoring the code. Extracing the calibration and initial setup processes into separate functions

--- pid.py
+++ pid.py
@@ -1,32 +1,19 @@
 #!/usr/bin/env python3
 import ev3dev.ev3 as ev3
-
-right = ev3.LargeMotor('outC')
-left = ev3.LargeMotor('outB')
-right.polarity = ev3.LargeMotor.ENCODER_POLARITY_NORMAL
-left.polarity = ev3.LargeMotor.ENCODER_POLARITY_NORMAL
-colorL = ev3.ColorSensor('in1')
-colorR = ev3.ColorSensor('in2')
-assert colorL.connected, "Connect a color sensor to port 1."
-assert colorR.connected, "Connect a color sensor to port 2."
-colorL.mode = 'COL-REFLECT'
-colorR.mode = 'COL-REFLECT'
+from config import Config
 
 
-def main():
-    integral = 0
-    prev_error = 0
+def calibrate(robot):
+    robot.right_motor.run_forever(speed_sp=50)
+    previous_r = robot.color_right.value()
     lowest_l = 100
     highest_l = 0
     lowest_r = 100
     highest_r = 0
 
-    right.run_forever(speed_sp=50)
-    previous_r = colorR.value()
-    integrations = 0
     while True:
-        current_r = colorR.value()
-        current_l = colorL.value()
+        current_r = robot.color_right.value()
+        current_l = robot.color_left.value()
         if current_l >= highest_l:
             highest_l = current_l
             print(current_l, "left")
@@ -37,35 +24,44 @@ def main():
             print(current_r, "right")
         elif current_r <= lowest_r:
             lowest_r = current_r
-        if current_r / previous_r > 1.05 and integrations >= 1:
-            right.stop()
+        if current_r / previous_r > 1.05:
+            robot.right_motor.stop()
             break
         previous_r = current_r
-        integrations += 1
 
     print(lowest_l, lowest_r, highest_l, highest_r, integrations)
-    thr_l = (lowest_l + highest_l) / 2
-    thr_r = (lowest_l + highest_l) / 2
-    right.polarity = ev3.LargeMotor.ENCODER_POLARITY_INVERSED
-    right.run_forever(speed_sp=50)
-    left.run_forever(speed_sp=50)
+    threshold = {"left": (lowest_l + highest_l) / 2, "right": (lowest_l + highest_l) / 2}
+    return threshold
+
+
+def align(robot, thershold):
+    robot.right_motor.polarity = ev3.LargeMotor.ENCODER_POLARITY_INVERSED
+    robot.right_motor.run_forever(speed_sp=50)
+    robot.left_motor.run_forever(speed_sp=50)
 
     while True:
-        current_l = colorL.value()
-        if thr_l * 0.95 <= current_l <= thr_l * 1.05:
-            print(current_l, thr_l)
-            right.polarity = ev3.LargeMotor.ENCODER_POLARITY_NORMAL
-            right.stop()
-            left.stop()
+        current_l = robot.color_left.value()
+        if thershold["left"] * 0.95 <= current_l <= thershold["left"] * 1.05:
+            print(current_l, thershold["left"])
+            robot.right_motor.polarity = ev3.LargeMotor.ENCODER_POLARITY_NORMAL
+            robot.right_motor.stop()
+            robot.left_motor.stop()
             break
+
+
+def main():
+    robot = Config()
+    threshold = calibrate(robot)
+
+    align(robot, threshold)
 
     while True:
         kp = 8
         ki = 0
         kd = 10
         ks = 1050
-        error_l = thr_l - colorL.value()
-        error_r = thr_r - colorR.value()
+        error_l = threshold["left"] - robot.color_left.value()
+        error_r = threshold["right"] - robot.color_right.value()
         error = error_l - error_r
         integral = integral + error
         differential = prev_error - error
@@ -76,20 +72,20 @@ def main():
         motorpow_l = base_speed - (proportional + integral_output + differential_output)
         motorpow_r = base_speed + proportional + integral_output + differential_output
         if motorpow_l < 0:
-            left.polarity = ev3.LargeMotor.ENCODER_POLARITY_INVERSED
+            robot.left_motor.polarity = ev3.LargeMotor.ENCODER_POLARITY_INVERSED
         else:
-            left.polarity = ev3.LargeMotor.ENCODER_POLARITY_NORMAL
+            robot.left_motor.polarity = ev3.LargeMotor.ENCODER_POLARITY_NORMAL
         if motorpow_r < 0:
-            right.polarity = ev3.LargeMotor.ENCODER_POLARITY_INVERSED
+            robot.right_motor.polarity = ev3.LargeMotor.ENCODER_POLARITY_INVERSED
         else:
-            right.polarity = ev3.LargeMotor.ENCODER_POLARITY_NORMAL
-        right.run_forever(speed_sp=abs(motorpow_r))
-        left.run_forever(speed_sp=abs(motorpow_l))
+            robot.right_motor.polarity = ev3.LargeMotor.ENCODER_POLARITY_NORMAL
+        robot.right_motor.run_forever(speed_sp=abs(motorpow_r))
+        robot.left_motor.run_forever(speed_sp=abs(motorpow_l))
         prev_error = error
 
 try:
     main()
 except:
-    right.stop()
+    robot.stop()
     left.stop()
     raise
